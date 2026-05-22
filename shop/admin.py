9f9234c72154c44a53dcd8a71c7aefac96bcb296
k1slee <trofimcuk.nikita10@gmail.com>
2026-05-22 16:17:36 +0300

    актуальная бд

--- shop/admin.py
+++ shop/admin.py
@@ -18,7 +18,7 @@ class ProductForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         if self.instance and self.instance.pk:
-            self.fields['in_stock'].initial = self.instance.stock > 0
+            self.fields['in_stock'].initial = getattr(self.instance, 'in_stock', False)
 
     def save(self, commit=True):
         product = super().save(commit=False)
